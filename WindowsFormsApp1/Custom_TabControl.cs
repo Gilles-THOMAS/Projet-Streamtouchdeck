3ed9eb6a6371c2b4be17ce23077651c6014b9049
Gilles-THOMAS <99942700+Gilles-THOMAS@users.noreply.github.com>
2022-02-18 06:32:10 +0100

    First real commit

---
+++ WindowsFormsApp1/Custom_TabControl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    class Custom_TabControl : TabControl
+    {
+        public Custom_TabControl()
+        {
+
+        }
+        public Custom_TabControl ShallowCopy()
+        {
+            return (Custom_TabControl)this.MemberwiseClone();
+        }
+    }
+}
